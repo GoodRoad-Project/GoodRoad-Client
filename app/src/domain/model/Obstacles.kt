d3a38cc1ee46ac6ff4e541a52e5691ac2acdad89
KseniaG8 <gorodkovaksenia84@mail.ru>
2026-03-11 01:32:08 +0300

    feat(model): add domain models

---
+++ app/src/domain/model/Obstacles.kt
@@ -0,0 +1,36 @@
+package com.example.goodroad.domain.model
+
+data class Obstacle(
+    val id: String,
+    val position: LocationPoint,
+    val type: ObstacleType,
+    val details: ObstacleDetails
+)
+
+enum class ObstacleType{
+    CURB,
+    STAIRS,
+    ROAD_SLOPE,
+    POTHOLES,
+    SAND,
+    GRAVEL;
+}
+
+sealed class ObstacleDetails {
+
+    data class Stairs(
+        val stepCount: Int,
+        val hasRamp: Boolean = false;
+    ) : ObstacleDetails()
+
+    data class Slope(
+        val angleDegrees: Double,
+    ) : ObstacleDetails()
+
+    data class Curb(
+        val heightCm: Int,
+        val hasRampCut: Boolean = false
+    ) : ObstacleDetails()
+
+}
+
