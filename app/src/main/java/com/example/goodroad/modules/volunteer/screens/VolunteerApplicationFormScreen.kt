c24020aa3e7ab72ff377ab97f66f70046ed8e6f0
SanriaArgos <argosi.sanria@gmail.com>
2026-09-13 01:32:50 +0300

    fix: volunteer certificate preview

--- app/src/main/java/com/example/goodroad/modules/volunteer/screens/VolunteerApplicationFormScreen.kt
+++ app/src/main/java/com/example/goodroad/modules/volunteer/screens/VolunteerApplicationFormScreen.kt
@@ -1,6 +1,5 @@
 package com.example.goodroad.ui.volunteer.screens
 
-import android.content.Context
 import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -14,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
@@ -270,6 +270,7 @@ fun VolunteerApplicationFormScreen(
                                 contentDescription = null,
                                 modifier = Modifier
                                     .height(90.dp)
+                                    .width(90.dp)
                                     .clip(RoundedCornerShape(12.dp)),
                                 contentScale = ContentScale.Crop
                             )
